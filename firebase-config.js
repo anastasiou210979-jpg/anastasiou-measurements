ced46eec1b2db2941960c13a88b2170dcdb21aa0
anastasiou210979-jpg <anastasiou210979@gmail.com>
2026-07-28 14:48:42 +0300

    ANASTASIOU Measurements v5.1 Firebase sync

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBAbojIR8lCC_wd1Rg8JwevzWcXtHPEWRc",
+  authDomain: "anastasiou-measurements.firebaseapp.com",
+  projectId: "anastasiou-measurements",
+  storageBucket: "anastasiou-measurements.firebasestorage.app",
+  messagingSenderId: "947143105529",
+  appId: "1:947143105529:web:8be4ae0e6a93e85215fb28"
+};
--- sw.js
+++ sw.js
@@ -1,25 +1,5 @@
-const CACHE='anastasiou-metriseis-final-v5-1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./logo.png','./icon-180.png','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-});
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch',event=>{
-  if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      return response;
-    }).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-    return response;
-  })));
-});
+const CACHE="anastasiou-v5-1-20260728";
+const ASSETS=["./","./index.html","./app.js","./firebase-config.js","./manifest.webmanifest"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
